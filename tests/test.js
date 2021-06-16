2de3864eaae4e79c1622c66467589d8f55a941d3
dwikyls <kreuzvernicterlanze@gmail.com>
2021-06-16 18:45:27 +0700

    add delete patch post test

--- tests/test.js
+++ tests/test.js
@@ -5,15 +5,21 @@ const app = require('../index');
 const app1 = require('../index');
 const sinon = require("sinon");
 const express = require('express');
-const calculator = require('../calculator')
-const nock = require('nock')
+const calculator = require('../calculator');
+const nock = require('nock');
+
+const fs = require('fs');
+const path = require('path');
+const pathToFile = path.resolve("./data.json");
+const getResources = () => JSON.parse(fs.readFileSync(pathToFile));
 
 chai.use(chaiHttp);
 chai.use(chaiNock);
 const should = chai.should();
-const expect = chai.expect
+const expect = chai.expect;
 
 const slug = 'lalala';
+const baseUrl = 'http://localhost:3001/api';
 
 describe("Using Sinon", () => {
     describe("Stub", () => {
@@ -35,6 +41,67 @@ describe("Using Sinon", () => {
             const result = await chai.request('http://127.0.0.1:3001').get('/api/test-slug/asd')
             expect(result.status).to.equal(400);
             expect(result.body).to.deep.equal({ message : 'Hello asdasd'});
+            nock.cleanAll()
+        })
+    })
+    
+    describe("/api/activeresource", () => {
+        it ("Should return OK", async () => {
+            nock('http://127.0.0.1:3001')
+                .get('/api/activeresource')
+                .reply(200, { message: 'OK' })
+
+            const result = await chai.request('http://127.0.0.1:3001').get('/api/activeresource')
+            expect(result.status).to.equal(200);
+            expect(result.body).to.deep.equal({ message : 'OK'});
+            nock.cleanAll()
+        })
+
+        it ("Should return NOT OK", async () => {
+            nock('http://127.0.0.1:3001')
+                .get('/api/activeresource')
+                .reply(400, { message: 'NOT OK' })
+
+            const result = await chai.request('http://127.0.0.1:3001').get('/api/activeresource')
+            expect(result.status).to.equal(400);
+            expect(result.body).to.deep.equal({ message : 'NOT OK'});
+            nock.cleanAll()
+        })
+    })
+
+    describe("/api/resources", () => {
+        it ("Should return OK", async () => {
+            nock('http://127.0.0.1:3001')
+                .get('/api/resources')
+                .reply(200, {
+                    message: 'OK', 
+                    data: [
+                        {
+                            id: 1,
+                            title: 'ok',
+                        },
+                        {
+                            id: 2,
+                            title: 'asd',
+                        }
+                    ]
+                })
+
+            const result = await chai.request('http://127.0.0.1:3001').get('/api/resources')
+            expect(result.status).to.equal(200);
+            expect(result.body).to.deep.equal({
+                message: 'OK', 
+                data: [
+                    {
+                        id: 1,
+                        title: 'ok',
+                    },
+                    {
+                        id: 2,
+                        title: 'asd',
+                    }
+                ]
+            });
             nock.cleanAll()
         })
     })
@@ -206,4 +273,87 @@ describe("/api", () => {
                 });
         });
     })
+
+    describe("Post /api/resources", () => {
+        it('Should apa ya', (done) => {
+            let asd = {
+                "title": "lorem ipsum 1",
+                "description": "lorem ipsum description 1",
+                "link": "https://link.com",
+                "priority": 3,
+                "timeToFinish": 12,
+              };
+            
+              chai.request(baseUrl)
+                .post(`/resources`)
+                .send(asd)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    done();
+                });
+        });
+    });
+
+    describe("Delete /api/resources:id", () => {
+        it('Should deleted successfully', (done) => {
+              chai.request(baseUrl)
+                .delete(`/resources/1623839843910`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    done();
+                });
+        });
+    });
+
+    describe("Patch /api/resources/:id", () => {
+        const resources = getResources();
+            let completeResource = resources.find(resource => resource.status === "complete");            
+            let inactiveResource = resources.find(resource => resource.status === "inactive");
+            let activeResource = resources.find(resource => resource.status === "active");
+            let asd = {
+                "title": "lorem ipsum 1",
+                "description": "lorem ipsum description 1",
+                "link": "https://link.com",
+                "priority": 3,
+                "timeToFinish": 12,
+              };
+
+        it('Should updated successfully', (done) => {            
+              chai.request(baseUrl)
+                .patch(`/resources/${inactiveResource.id}`)
+                .send(asd)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('errors');
+                    done();
+                });
+        });
+
+        it('Should cannot update because resource has been completed', (done) => {            
+              chai.request(baseUrl)
+                .patch(`/resources/${completeResource.id}`)
+                .send(asd)
+                .end((err, res) => {
+                    res.should.have.status(422);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('errors');
+                    done();
+                });
+        });
+
+        it('Should already active resource ', (done) => {            
+            chai.request(baseUrl)
+              .patch(`/resources/${completeResource.id}`)
+              .send(asd)
+              .end((err, res) => {
+                  res.should.have.status(422);
+                  res.body.should.be.a('object');
+                  res.body.should.have.property('errors');
+                  done();
+              });
+      });
+    });
 })
